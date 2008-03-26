5985db80d5255033bd5772cd377e04937c0cc448
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2008-03-26 09:54:13 +0000

    moved because of updated module folder structure

---
+++ pninit.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * initialise the MyProfile module
+ *
+ * @return       bool       true on success, false otherwise
+ */
+function MyProfile_init()
+{
+  	if (!DBUtil::createTable('myprofile')) return false;
+  	if (!DBUtil::createTable('myprofile_fields')) return false;
+  	if (!DBUtil::createTable('myprofile_settings')) return false;
+  	if (!DBUtil::createTable('myprofile_emailverification')) return false;
+
+    // Module Variables
+    $notabs = pnSessionGetVar('myprofile_notabs');
+    $noverification = pnSessionGetVar('myprofile_noverification');
+    pnModSetVar('MyProfile', 'notabs', (($notabs<>false) ? $notabs : ''));	
+    pnModSetVar('MyProfile', 'noverification', (($noverification<>false) ? $noverification : ''));	
+
+    // clean up
+    pnSessionDelVar('myprofile_notabs');
+    pnSessionDelVar('myprofile_noverification');
+			
+    // delete old config file if there is one
+    $configfile = 'modules/MyProfile/config/tabledef.inc';
+    if (file_exists($configfile)) unlink($configfile);
+
+    // Initialisation successful
+    return true;
+}
+
+/**
+ * delete the MyProfile module
+ *
+ * @return       bool       true on success, false otherwise
+ */
+function MyProfile_delete()
+{
+  	if (!DBUtil::dropTable('myprofile')) return false;
+  	if (!DBUtil::dropTable('myprofile_fields')) return false;
+  	if (!DBUtil::dropTable('myprofile_settings')) return false;
+  	if (!DBUtil::dropTable('myprofile_emailverification')) return false;
+
+    // Delete any module variables
+    pnModDelVar('MyProfile');
+    
+    // delete old config file if there is one
+    $configfile = 'modules/MyProfile/config/tabledef.inc';
+    if (file_exists($configfile)) unlink($configfile);
+
+    // Deletion successful
+    return true;
+}
+
+function MyProfile_upgrade($oldversion)
+{
+    return true;
+}
+?>
