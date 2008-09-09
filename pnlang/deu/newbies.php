b67dae4b17b53e1d612a8c26ac65506bbd4e1bbd
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2008-09-09 19:21:25 +0000

    Added block: last registrations, #24: http://code.zikula.org/myprofile/ticket/24

---
+++ pnlang/deu/newbies.php
@@ -0,0 +1,5 @@
+<?php
+define('_MYPROFILENEWUSERS', 'Aktuelle Neuankömmlinge');
+define('_MYPROFILEWELCOMETOOURCOMMUNITY', 'Willkommen in unserer Community!');
+define('_MYPROFILENUMBERSOFNEWUSERSTOSHOW', 'Anzahl der anzuzeigenden frisch registrierten Benutzer');
+?>
