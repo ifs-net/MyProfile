874fd766f99a4fcf30208ac8b482ec159a250f1f
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2008-09-09 19:39:04 +0000

    common function lib that will be included in all ifs-net.de modules later ...

---
+++ modules/MyProfile/includes/lib_ifs-net.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package      iFS-net.de modules -- common function library
+ * @version      $Id $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+ 
+ 
+?>
