23ff0df4f6c4ccc45d7ca48553155ed55328c99f
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2008-10-03 14:00:12 +0000

    changed formatting...

--- modules/MyProfile/pnlang/deu/birthday.php
+++ modules/MyProfile/pnlang/deu/birthday.php
@@ -1,9 +1,9 @@
 <?php
-define('_MYPROFILEDATEDATAFIELD', 'Datenbankfeld in welchem das Geburtsdatum eines Benutzers im Format YYY-MM-DD (DATE) gespeichert ist');
-define('_MYPROFILERESTRICTIONDATAFIELD', 'Restriktionsfeld, wenn z.B. ein Feld im Profil existiert, ob ein Benutzer überhaupt den Geburtstag öffentlich dargestellt haben will');
-define('_MYPROFILERESTRICTIONDATAFIELDVALUE', 'Wert des Restriktionsfelds für den Fall, dass der Geburtstag im Block angezeigt werden soll');
-define('_MYPROFILEDATEDATAFIELDVALUEOPTION', '-- nichts ausgewählt --');
+define('_MYPROFILEDATEDATAFIELD', 					'Datenbankfeld in welchem das Geburtsdatum eines Benutzers im Format YYY-MM-DD (DATE) gespeichert ist');
+define('_MYPROFILERESTRICTIONDATAFIELD', 			'Restriktionsfeld, wenn z.B. ein Feld im Profil existiert, ob ein Benutzer überhaupt den Geburtstag öffentlich dargestellt haben will');
+define('_MYPROFILERESTRICTIONDATAFIELDVALUE',		'Wert des Restriktionsfelds für den Fall, dass der Geburtstag im Block angezeigt werden soll');
+define('_MYPROFILEDATEDATAFIELDVALUEOPTION', 		'-- nichts ausgewählt --');
 define('_MYPROFILERESTRICTIONDATAFIELDVALUEOPTION', '-- nichts ausgewählt. Leer lassen wenn keine Restriktion verwendet werden soll --');
-define('_MYPROFILEBIRTHDAYS', 'Unsere Geburtstagskinder');
-define('_MYPROFILEHAPPYBIRTHDAY', 'alles Gute im neuen Lebensjahr!');
+define('_MYPROFILEBIRTHDAYS', 						'Unsere Geburtstagskinder');
+define('_MYPROFILEHAPPYBIRTHDAY', 					'alles Gute im neuen Lebensjahr!');
 ?>
