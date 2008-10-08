4f07fca0ad65b1d429730100b085dc956e31ab8f
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2008-10-08 16:47:40 +0000

    improved file structure: classes (pnform handlers) exluded in extra files now

---
+++ includes/classes/admin/settings.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package      MyProfile
+ * @version      $Id: $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+class MyProfile_admin_settingsHandler
+{
+    function initialize(&$render)
+    {
+	  	$data['allowmemberlist']		= pnModGetVar('MyProfile','allowmemberlist');
+	  	$data['notabs'] 				= pnModGetVar('MyProfile','notabs');
+	  	$data['individualpermissions'] 	= pnModGetVar('MyProfile','individualpermissions');
+	  	$data['individualtemplates'] 	= pnModGetVar('MyProfile','individualtemplates');
+	  	$data['plugin_noajax'] 			= pnModGetVar('MyProfile','plugin_noajax');
+	  	$data['validuntil'] 			= pnModGetVar('MyProfile','validuntil');
+	  	$data['asattributes']			= pnModGetVar('MyProfile','asattributes');
+	  	$data['dateformat'] 			= pnModGetVar('MyProfile','dateformat');
+	  	$data['noverification']			= pnModGetVar('MyProfile','noverification');
+	  	$data['requestban'] 			= pnModGetVar('MyProfile','requestban');
+	  	$data['expiredays'] 			= pnModGetVar('MyProfile','expiredays');
+	  	$data['searchtemplate']			= pnModGetVar('MyProfile','searchtemplate');
+	  	$data['resultsperpage']			= pnModGetVar('MyProfile','resultsperpage');
+	  	$groups	= pnModAPIFunc('MyProfile','admin','getGroupsConfiguration');
+	  	$groups_list = array();
+	  	foreach ($groups as $g) $groups_list[] = array('text' => $g['name'], 'value' => $g['gid']);
+		$data['groups'] = $groups_list;
+		$data['disabledgroups'] = unserialize(pnModGetVar('MyProfile','disabledgroups'));
+	  	$render->assign($data);
+		return true;
+    }
+    function handleCommand(&$render, &$args)
+    {
+		if ($args['commandName']=='update') {
+		    // Security check 
+		    if (!SecurityUtil::checkPermission('MyProfile::', '::', ACCESS_ADMIN)) return LogUtil::registerPermissionError();
+
+			// get the pnForm data and do a validation check
+		    $obj = $render->pnFormGetValues();		    
+		    if (!$render->pnFormIsValid()) return false;
+		    // store all passed form values as module variables
+		    foreach ($obj as $key=>$value) {
+		      	if (is_array($value)) $value=serialize($value);
+			  	pnModSetVar('MyProfile',(string)$key,(string)$value);
+			}
+			LogUtil::registerStatus(_MYPROFILECFGSTORED);
+		}
+		return true;
+    }
+}
---
+++ includes/classes/user/settings.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @package      MyProfile
+ * @version      $Id: $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+ 
+class MyProfile_user_SettingsHandler
+{
+	var $id;
+	function initialize(&$render)
+	{	    
+		// fields needed for individual templating
+		$individualtemplates = (int)pnModGetVar('MyProfile','individualtemplates');
+		if ($individualtemplates == 1) {
+			$fields = pnModAPIFunc('MyProfile','admin','getIndividualTemplateFields');
+			$render->assign('fields', $fields);
+		}
+		
+		// should just password or email management be shown?
+		$mode = strtolower(FormUtil::getPassedValue('mode'));
+		$render->assign('mode', $mode);
+		$render->assign('noverification', pnModGetVar('MyProfile','noverification'));
+	
+		$this->id = (int)pnUserGetVar('uid');
+		if ($this->id > 0) {
+			// get settings
+			$data = pnModAPIFunc('MyProfile','user','getSettings',array('uid'=>$this->id));
+			
+			$individualtemplates = (int)pnModGetVar('MyProfile','individualtemplates');
+			if (($individualtemplates == 1) && (pnModAPIFunc('MyProfile','user','individualTemplateAllowed',array('uid' => $this->id)))) $render->assign('individualtemplates',1);
+			$render->assign($data);
+			// individual permission settings
+			$render->assign('individualpermissions',	pnModGetVar('MyProfile','individualpermissions'));
+			$items_individualpermissions = array (
+					array('text' => _MYPROFILEALL, 		'value' => 0),
+					array('text' => _MYPROFILEMEMBERS, 	'value' => 1)
+				);
+			if (pnModAvailable('ContactList')) $items_individualpermissions[] = array('text' => _MYPROFILEBUDDIES, 	'value' => 2);
+			$render->assign('items_individualpermissions', $items_individualpermissions);
+			// are there custom settings we should take care of?
+			$fields = pnModAPIFunc('MyProfile','admin','getFields');
+			$customfields = false;
+			foreach ($fields as $field) {
+			  	if ($field['public_status'] == 9) $customfields = true;
+			}
+			if ($customfields) {
+				$contactlistavailable = pnModAvailable('ContactList');
+				$items_customfields[] = array('text' => _MYPROFILEALL, 'value' => 0);
+				$items_customfields[] = array('text' => _MYPROFILEMEMBERS, 'value' => 1);
+				if ($contactlistavailable) $items_customfields[] = array('text' => _MYPROFILEBUDDIES, 'value' => 2);
+				$items_customfields[] = array('text' => _MYPROFILELISTEDUSERSONLY, 'value' => 3);
+				$render->assign('items_customfields', $items_customfields);
+				// assign user's value
+				$render->assign('customsettings',	$data['customsettings']);
+			}
+		}
+		return true;
+    }
+	function handleCommand(&$render, &$args)
+	{
+		if ($args['commandName']=='update') {
+		    // Security check 
+		    if (!SecurityUtil::checkPermission('MyProfile::', '::', ACCESS_COMMENT)) return LogUtil::registerPermissionError();
+
+			// get the pnForm data and do a validation check
+		    $obj = $render->pnFormGetValues();		    
+		    if (!$render->pnFormIsValid()) return false;
+
+			// if individualtemplate is on and template is submitted we have to validate for mandatory fields
+			$individualtemplates = (int)pnModGetVar('MyProfile','individualtemplates');
+			if (($individualtemplates == 1) && isset($obj['individualtemplate']) && ($obj['individualtemplate'] != "")) {
+				if (!pnModAPIFunc('MyProfile','admin','validateIndividualTemplate',array('template' => $obj['individualtemplate']))) {
+					LogUtil::registerError(_MYPROFILETEMPLATEINCLUDEMANDATORY);
+					return false;
+				}
+			}
+
+	      	$obj['id']=$this->id;
+			$result = pnModAPIFunc('MyProfile','user','setSettings',array(
+					'uid'					=> $obj['id'],
+					'nocomments' 			=> $obj['nocomments'],
+					'individualpermission' 	=> $obj['individualpermission'],
+					'individualtemplate' 	=> $obj['individualtemplate'],
+					'customsettings'	 	=> $obj['customsettings']
+					)
+				);
+	      	if ($result) LogUtil::registerStatus(_MYPROFILESETTINGSUPDATED);
+
+	      	// should the password be updated?
+	      	if (isset($obj['1pass']) && ($obj['2pass'] == $obj['2pass'])) {
+	      	  	if (strlen($obj['1pass'])>3) {
+					 pnUserSetPassword($obj['1pass']); 
+					 LogUtil::registerStatus(_MYPROFILEPASSWORDCHANGED);
+				}
+				else if (strlen($obj['1pass'])>0) LogUtil::registerError(_MYPROFILEPWDTOOSHORT);
+			}
+			else if ((strlen($obj['1pass'])>0) || (strlen($obj['2pass'])>0)) {
+			  	LogUtil::registerError(_MYPROFILEPASSWORDSINCORRECT);
+			}
+			
+			// should the emailadress be changed?
+			if (($obj['users_email'] != '') && (pnUserGetVar('email') != $obj['users_email'])){
+			  	if (pnModGetVar('MyProfile','noverification') == '1') {	// change without any verification
+				    if (pnUserSetVar('email',$obj['users_email'])) LogUtil::registerStatus(_MYPROFILEEMAILCHANGED);
+				    else LogUtil::registerStatus(_MYPROFILEEMAILCHANGEERROR);
+				}
+			  	else {
+			  	  	// generate verification code etc.
+			  	  	if (pnModAPIFunc('MyProfile','user','generateVerificationCode',array('email' => $obj['users_email']))) LogUtil::registerStatus(_MYPROFILEMAILCHANGEREQUEST);
+			  	  	else LogUtil::registerStatus(_MYPROFILEMAILCHANGEREQUESTERROR);
+				}
+			}
+			return pnRedirect(pnModURL('MyProfile','user','settings'));
+		}
+		return true;
+    }
+}
