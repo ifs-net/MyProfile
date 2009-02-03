4e0c7a25b25738726b792aad8663f04e61e0a93a
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2009-02-03 16:19:13 +0000

    tagging 1.2 release

---
+++ config/plugins/function.myprofileinvalidemail.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package      MyProfile
+ * @version      $Id: function.myprofileinvalidemail.php 270 2009-01-11 21:37:53Z quan $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+ 
+/**
+ * forces users having an invalid email address to update the email address
+ *
+ */
+function smarty_function_myprofileinvalidemail()
+{
+    if (pnUserLoggedIn()
+	&& pnModAvailable('MyProfile')
+	&& (pnModGetName() != 'MyProfile')
+	&& (strtolower(FormUtil::getPassedValue('type') 		!= 'admin'		))) {
+	  	$attributes = pnUserGetVar('__ATTRIBUTES__');
+	  	if ($attributes['myprofile_invalidemail'] == 1) {
+		    // user has invalid email address
+      	  	// load language file
+      	  	pnModLangLoad('MyProfile','plugin');
+      	  	// register error message
+		    LogUtil::registerError(_MYPROFILEYOUREMAILINVALID);
+		    return pnRedirect(pnModURL('MyProfile','user','email'));
+		}
+    }
+}
+?>
---
+++ config/plugins/function.myprofilemandatory.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package      MyProfile
+ * @version      $Id: function.myprofilemandatory.php 273 2009-01-11 21:54:28Z quan $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+ 
+/**
+ * force users to create a mandatory profile
+ *
+ * A user's profile is mandatory and valid if 
+ * $userdata['__ATTRIBUTES__']['validuntil']	timestamp
+ * is not older than today's unix timestamp.
+ * Otherwise the profile has to be updated. The user will be re-
+ * directed to the profile management otherwise.
+ */
+function smarty_function_myprofilemandatory()
+{
+    if (pnUserLoggedIn()
+	&& pnModAvailable('MyProfile')
+	&& (pnModGetName() != 'MyProfile')
+	&& (strtolower(FormUtil::getPassedValue('type') 		!= 'admin'		))) {
+	  	$attributes = pnUserGetVar('__ATTRIBUTES__');
+	  	// first check if email address is marked as invalid
+	  	if ($attributes['myprofile_invalidemail'] == 1) {
+		    // user has invalid email address
+      	  	// load language file
+      	  	pnModLangLoad('MyProfile','plugin');
+      	  	// register error message
+		    LogUtil::registerError(_MYPROFILEYOUREMAILINVALID);
+		    return pnRedirect(pnModURL('MyProfile','user','settings',array('mode' => 'email')));
+		}
+	  	else if (!pnModAPIFunc('MyProfile','user','hasValidProfile')) {
+      	  	// load language file
+      	  	pnModLangLoad('MyProfile','plugin');
+      	  	// register error message
+		    LogUtil::registerError(_MYPROFILEPROFILEOUTOFTIME);
+		    return pnRedirect(pnModURL('MyProfile','user','main'));
+		}
+    }
+}
+?>
