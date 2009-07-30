73caeaba04838b5e1e06e4032da153688aacdeb7
quan <quan@0d4254a5-d588-431c-b745-ca6fd62d38e8>
2009-07-30 17:13:52 +0000

    fixes #73 and implements an notification for site admin when new users are created

---
+++ includes/common_admin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package      MyProfile
+ * @version      $Id: $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2009
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */ 
+ 
+function mp_admin_sendNotification($obj) {
+  	$notification = pnModGetVar('MyProfile','notification');
+  	if ($notification == 1) {
+	  	// get profile
+	  	$profile = pnModAPIFunc('MyProfile','user','getProfile',array('uid' => $obj['id']));
+		// Get vars
+	    $sitename 	= pnConfigGetVar('sitename');
+	    $to 		= pnConfigGetVar('adminmail');
+	    $subject = _MYPROFILENEWPROFILE.' '._MYPROFILEAT.' '.$sitename;
+		$body = pnUserGetVar('uname',$obj['id'])." - ".pnUserGetVar('email',$obj['id'])."\n\n";
+		foreach ($profile as $p) {
+		  	if ($p['fieldtype'] != 'COORD') {
+				$body.=$p['description'].": ".$p['value']."\n";
+			} 
+		}
+		pnMail($to, $subject, $body, array('header' => '\nMIME-Version: 1.0\nContent-type: text/plain'), false);
+	}
+    return true;
+}
